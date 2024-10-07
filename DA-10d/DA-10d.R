e6252e8b750fbe4f4a28570cbb6f6e0b733a1fe8
varshhvarsh <varshinisathish123@gmail.com>
2024-10-07 22:12:25 +0530

    Add files via upload

---
+++ DA-10d/DA-10d.R
@@ -0,0 +1,9 @@
+# Load the ggplot2 package
+library(ggplot2)
+# Box plot of Sepal Length for each Species
+ggplot(data = iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
+  geom_boxplot() + # Adds box plot
+  labs(title = "Box Plot of Sepal Length by Species",
+       x = "Species",
+       y = "Sepal Length (cm)") +
+  theme_minimal()
